cb6513fde6eb9cdfe7bbb2a0ca84221d6971d296
Pavel Marek <pmarek@redhat.com>
2025-03-06 19:42:51 +0100

    thins

--- ooo.py
+++ ooo.py
@@ -2,17 +2,33 @@ from ollama import chat
 from ollama import ChatResponse
 from llama_index.core import SimpleDirectoryReader
 
-documents = SimpleDirectoryReader("./data").load_data()
+# only load markdown files
+required_exts = [".sh"]
+
+reader = SimpleDirectoryReader(
+    input_dir="./data/repos/kiali-master",
+    required_exts=required_exts,
+    recursive=True,
+)
+
+docs = reader.load_data()
+print(f"Loaded {len(docs)} docs")
+
+aggregate_scripts = ''
+
+for doc in docs:
+  aggregate_scripts += doc.text
+
+print(aggregate_scripts)
 
 response: ChatResponse = chat(model='llama3.3:70b-instruct-q2_K', messages=[
   {
     'role': 'user',
-    'content': 'Why is the sky blue? Give me super short answer.',
+    # 'content': 'Help me write new testcase in cypress for a kiali. test case should respect existing form and functions. It should be atomic and test only one thing using BDD approach as it is in examples. The test and sceanrio needs to verify if control plane informations are dispalyed in mesh page view. use only already existing codebase and fuctions and nothing else',
+    'content': 'suggest best practicies in probided context of bash scripts: ' + aggregate_scripts  
   },
 ])
 
 
 print(response['message']['content'])
-# or access fields directly from the response object
-print(response.message.content)
 
