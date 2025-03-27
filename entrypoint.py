86bd4d22e535b2ef64317eb2bac6c6fac3789bda
Pavel Marek <pmarek@redhat.com>
2025-03-27 02:36:19 +0100

    naming

---
+++ entrypoint.py
@@ -0,0 +1,10 @@
+# import yaml
+
+# # Load YAML config
+# with open("config.yaml") as f:
+#     cfg = yaml.safe_load(f)
+
+# print(cfg["ollama"]["model"], cfg["db"])
+
+# from ml.embbed_and_store import init_db  
+# init_db(cfg["db"], cfg["embedding"]["model"])
